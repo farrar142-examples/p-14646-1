bda3174068e7292ed16ef921f6a0e825f3040ae2
farrar142 <gksdjf1690@gmail.com>
2025-12-17 22:20:05 +0900

    c016

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,7 +1,7 @@
 
 package com.back.domain.post.post.document;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.data.annotation.*;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -11,7 +11,7 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.time.OffsetDateTime;
 
 @Document(indexName = "posts")
-@Getter
+@Data
 public class Post {
     @Id
     private String id;
@@ -40,18 +40,6 @@ public class Post {
         this.author = author;
         this.createdAt = OffsetDateTime.now();
         this.lastModifiedAt = OffsetDateTime.now();
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", author='" + author + '\'' +
-                ", createdAt=" + createdAt +
-                ", lastModifiedAt=" + lastModifiedAt +
-                '}';
     }
 }
 
