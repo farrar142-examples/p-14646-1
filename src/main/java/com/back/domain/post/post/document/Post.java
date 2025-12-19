06b04d14f63dd0b1da4a3c24e04846a6ac4ba250
farrar142 <gksdjf1690@gmail.com>
2025-12-19 21:05:37 +0900

    c019

--- src/main/java/com/back/domain/post/post/document/Post.java
+++ src/main/java/com/back/domain/post/post/document/Post.java
@@ -1,21 +1,19 @@
 
 package com.back.domain.post.post.document;
 
+import com.back.global.BaseDocument;
 import lombok.Data;
-import org.springframework.data.annotation.*;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.OffsetDateTime;
-
 @Document(indexName = "posts")
 @Data
-public class Post implements Persistable<String> {
-    @Id
-    private String id;
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Post extends BaseDocument<String> {
     @Field(type= FieldType.Text)
     private String title;
     @Field(type= FieldType.Text)
@@ -23,19 +21,6 @@ public class Post implements Persistable<String> {
     @Field(type= FieldType.Keyword)
     private String author;
 
-    @Field(
-            type = FieldType.Date,
-            format = DateFormat.date_time
-    )
-    @CreatedDate
-    private OffsetDateTime createdAt;
-
-    @Field(
-            type = FieldType.Date,
-            format = DateFormat.date_time
-    )
-    @LastModifiedDate
-    private OffsetDateTime lastModifiedAt;
 
     public Post(String title, String content, String author){
         this.title = title;
@@ -43,10 +28,6 @@ public class Post implements Persistable<String> {
         this.author = author;
     }
 
-    @Override
-    public boolean isNew() {
-        return id == null || (createdAt == null && lastModifiedAt == null);
-    }
 }
 
 
