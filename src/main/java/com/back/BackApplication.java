79518ec03a116836030acb06bdccf8487b849f63
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2026-01-05 15:15:18 +0900

    0007

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 @EnableElasticsearchAuditing
 public class BackApplication {
-//0006
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
 	}
