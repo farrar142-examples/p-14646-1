697b1288d42ce4b7b1746d93ee2604bbf5077b19
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2026-01-05 15:03:18 +0900

    0001

--- src/main/java/com/back/global/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.back.global.exception;
-
-public class NotFoundException extends DomainException {
-    public NotFoundException(String message) {
-        super("404", message);
-    }
-}
